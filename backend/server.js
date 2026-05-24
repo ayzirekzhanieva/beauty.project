5f167fc61e28609daff70dec97060b49d4116857
aizirek <aizirek.zhanieva@gmail.com>
2026-05-24 07:04:10 +0600

    Allow Vercel domain in CORS

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://beauty-project.vercel.app",
+    "https://beauty-project-inky.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
